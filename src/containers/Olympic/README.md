a02942a7ccb5a45ff6ab9139e0fb88db412b2dc9
dragonir <dragonirxo@outlook.com>
2022-01-29 17:17:08 +0800

    feat: Olympic Container添加飘动的旗帜🚩，样式优化.

--- src/containers/Olympic/README.md
+++ src/containers/Olympic/README.md
@@ -1,4 +1,4 @@
-# Three.js实现3D冬奥会创意页面
+# Three.js实现3D冬奥主题会创意页面
 
 <!-- 迎冬奥向未来 -->
 
@@ -12,3 +12,19 @@ https://anyconv.com/tw/max-zhuan-obj/
 
 在blender2.8中怎么给模型贴图
 blender 模型贴图 https://jingyan.baidu.com/article/363872ecf6367f2f4ba16f95.html
+
+## 背景
+
+## 效果
+
+## 实现
+
+## 总结
+
+## 下期预告
+
+* 《Metahuman元人类！记一次Three.js人像优化》
+* 《y2k！3D千年虫风格，y2k的事你少管》
+* 《喜羊羊你可真厉害，沸羊羊我讨厌你》
+
+## 附录
--- src/containers/Olympic/index.js
+++ src/containers/Olympic/index.js
@@ -2,13 +2,18 @@
 import React from 'react';
 import Stats from "three/examples/jsm/libs/stats.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { TWEEN } from "three/examples/jsm/libs/tween.module.min.js";
+import Animations from '../../assets/utils/animations';
 import landModel from './models/land.glb';
-import pandaModel from './models/panda.gltf';
+import treeModel from './models/panda.gltf';
 import bingdundunModel from './models/bingdundun.glb';
+import flagModel from './models/flag.glb';
 import skyTexture from './images/sky.jpg';
 import snowTexture from './images/snow.png';
 import treeTexture from './images/tree.png';
+import flagTexture from './images/flag.png';
+require('./libs/GLTFLoader.js');
+import './index.css';
 
 export default class Olympic extends React.Component {
 
@@ -16,18 +21,22 @@ export default class Olympic extends React.Component {
     super(props);
   }
 
+  state = {
+    loadingProcess: 0
+  }
+
   componentDidMount() {
     this.initThree()
   }
 
   initThree = () => {
-    var container, controls, stats;
+    var container, controls, stats, mixer;
     var camera, scene, renderer, light, land = null, meshes = [], points = [];
-    var fiveCyclesGroup = new THREE.Group();
+    var fiveCyclesGroup = new THREE.Group(), clock = new THREE.Clock();
     var mouseX = 0, mouseY = 0;
     var windowHalfX = window.innerWidth / 2;
     var windowHalfY = window.innerHeight / 2;
-
+    var _this = this;
     init();
     animate();
     function init() {
@@ -41,47 +50,74 @@ export default class Olympic extends React.Component {
       scene = new THREE.Scene();
       scene.background = new THREE.TextureLoader().load(skyTexture);
       camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.set(0, -2, 20);
+      camera.position.set(0, 30, 100);
       camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-      // var axes = new THREE.AxisHelper(30);
-      // scene.add(axes);
-
       // 性能工具
-      stats = new Stats();
-      document.documentElement.appendChild(stats.dom);
+      // stats = new Stats();
+      // document.documentElement.appendChild(stats.dom);
 
       const cubeGeometry = new THREE.BoxGeometry(0.001, 0.001, 0.001);
       const cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xdc161a });
       const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-      cube.position.set(0, 0, 0,);
-      light = new THREE.DirectionalLight(0xdddddd, 1);
-      light.intensity = 1.2;
-      light.position.set(-5, 8, 3);
+      cube.position.set(0, 0, 0);
+      light = new THREE.DirectionalLight(0xffffff, 1);
+      light.intensity = 1;
+      light.position.set(16, 16, 8);
       light.castShadow = true;
       light.target = cube;
       light.shadow.mapSize.width = 512 * 12;
       light.shadow.mapSize.height = 512 * 12;
-      light.shadow.camera.top = 10;
-      light.shadow.camera.bottom = - 5;
-      light.shadow.camera.left = - 5;
-      light.shadow.camera.right = 10;
+      light.shadow.camera.top = 40;
+      light.shadow.camera.bottom = -40;
+      light.shadow.camera.left = -40;
+      light.shadow.camera.right = 40;
       scene.add(light);
 
-      var ambientLight = new THREE.AmbientLight(0xdddddd);
+      // const lightHelper = new THREE.DirectionalLightHelper(light, 1, 'red');
+      // scene.add(lightHelper);
+      // const lightCameraHelper = new THREE.CameraHelper(light.shadow.camera);
+      // scene.add(lightCameraHelper);
+
+      var ambientLight = new THREE.AmbientLight(0xcfffff);
+      ambientLight.intensity = 1;
       scene.add(ambientLight);
 
+      const manager = new THREE.LoadingManager();
+      manager.onStart = (url, loaded, total) => {};
+      manager.onLoad = () => { console.log('Loading complete!')};
+      manager.onProgress = async(url, loaded, total) => {
+        if (Math.floor(loaded / total * 100) === 100) {
+          _this.loadingProcessTimeout && clearTimeout(_this.loadingProcessTimeout);
+          _this.loadingProcessTimeout = setTimeout(() => {
+            _this.setState({ loadingProcess: Math.floor(loaded / total * 100) });
+            Animations.animateCamera(camera, controls, { x: 0, y: -1, z: 20 }, { x: 0, y: 0, z: 0 }, 3600, () => {});
+          }, 800);
+        } else {
+          _this.setState({ loadingProcess: Math.floor(loaded / total * 100) });
+        }
+      };
+
       // 添加地面
-      var loader = new GLTFLoader();
+      var loader = new THREE.GLTFLoader(manager);
       loader.load(landModel, function (mesh) {
         mesh.scene.traverse(function (child) {
           if (child.isMesh) {
             meshes.push(child)
+            child.material.metalness = .1;
+            child.material.roughness = .8;
+            // 地面
             if (child.name === 'Mesh_2') {
-              child.material.metalness = .1
-              child.material.roughness = .8
-              child.material.color = new THREE.Color(0xffffff)
+              child.material.metalness = .5;
+              child.receiveShadow = true;
             }
+            // 围巾
+            if (child.name === 'Mesh_17') {
+              child.material.metalness = .2;
+              child.material.roughness = .8;
+            }
+            // 帽子
+            if (child.name === 'Mesh_17') {}
           }
         });
         mesh.scene.rotation.y = Math.PI / 4;
@@ -91,6 +127,39 @@ export default class Olympic extends React.Component {
         scene.add(land);
       });
 
+      // 旗帜
+      loader.load(flagModel, mesh => {
+        mesh.scene.traverse(child => {
+          if (child.isMesh) {
+            meshes.push(child)
+            child.castShadow = true;
+            // 旗帜
+            if (child.name === 'mesh_0001') {
+              child.material.metalness = .1;
+              child.material.roughness = .1;
+              child.material.map = new THREE.TextureLoader().load(flagTexture);
+            }
+            // 旗杆
+            if (child.name === '柱体') {
+              child.material.metalness = .6;
+              child.material.roughness = 0;
+              child.material.refractionRatio = 1;
+              child.material.color = new THREE.Color(0xeeeeee);
+            }
+          }
+        });
+        mesh.scene.rotation.y = Math.PI / 24;
+        mesh.scene.position.set(2, -7, -1);
+        mesh.scene.scale.set(4, 4, 4);
+        // 动画
+        let meshAnimation = mesh.animations[0];
+        mixer = new THREE.AnimationMixer(mesh.scene);
+        let animationClip = meshAnimation;
+        let clipAction = mixer.clipAction(animationClip).play();
+        animationClip = clipAction.getClip();
+        scene.add(mesh.scene);
+      });
+
       // bingdundun
       loader.load(bingdundunModel, function (mesh) {
         mesh.scene.traverse(function (child) {
@@ -98,99 +167,84 @@ export default class Olympic extends React.Component {
             meshes.push(child)
             if (child.name === 'oldtiger001') {
               child.material.metalness = .5
+              child.material.roughness = .8
             }
             if (child.name === 'oldtiger002') {
               child.material.transparent = true;
               child.material.opacity = .5
+              child.material.metalness = .2
+              child.material.roughness = 0
+              child.material.refractionRatio = 1
+              child.castShadow = true;
             }
           }
         });
         mesh.scene.rotation.y = Math.PI / 24;
-        mesh.scene.position.set(-8, -10, 0);
+        mesh.scene.position.set(-8, -12, 0);
         mesh.scene.scale.set(24, 24, 24);
         scene.add(mesh.scene);
       });
 
       // 添加树
-      let pandaMaterial = new THREE.MeshPhysicalMaterial({
-          map: new THREE.TextureLoader().load(treeTexture),
-          transparent: true,
-          side: THREE.DoubleSide,
-          metalness: .2,
-          roughness: .8,
-          depthTest: true,
-          depthWrite: false,
-          skinning: false,
-          fog: false,
-          reflectivity: 0.1,
-          refractionRatio: 0,
+      let treeMaterial = new THREE.MeshPhysicalMaterial({
+        map: new THREE.TextureLoader().load(treeTexture),
+        transparent: true,
+        side: THREE.DoubleSide,
+        metalness: .2,
+        roughness: .8,
+        depthTest: true,
+        depthWrite: false,
+        skinning: false,
+        fog: false,
+        reflectivity: 0.1,
+        refractionRatio: 0,
       });
-      let pandaCustomDepthMaterial = new THREE.MeshDepthMaterial({
-          depthPacking: THREE.RGBADepthPacking,
-          map: new THREE.TextureLoader().load(treeTexture),
-          alphaTest: 0.5
+      let treeCustomDepthMaterial = new THREE.MeshDepthMaterial({
+        depthPacking: THREE.RGBADepthPacking,
+        map: new THREE.TextureLoader().load(treeTexture),
+        alphaTest: 0.5
       });
-      loader.load(pandaModel, function (mesh) {
+      loader.load(treeModel, function (mesh) {
         mesh.scene.traverse(function (child) {
           if (child.isMesh) {
             meshes.push(child)
-            child.material = pandaMaterial
-            child.custromMaterial = pandaCustomDepthMaterial
+            child.material = treeMaterial;
+            child.custromMaterial = treeCustomDepthMaterial;
           }
         });
-        mesh.scene.position.set(14, -8, 0);
+        mesh.scene.position.set(14, -9, 0);
         mesh.scene.scale.set(16, 16, 16);
         scene.add(mesh.scene);
         let tree2 = mesh.scene.clone();
-        tree2.position.set(8, -8, -14);
+        tree2.position.set(10, -8, -15);
         tree2.scale.set(18, 18, 18);
         scene.add(tree2)
-
         let tree3 = mesh.scene.clone();
-        tree3.position.set(-16, -8, -16);
-        tree3.scale.set(18, 18, 18);
+        tree3.position.set(-18, -8, -16);
+        tree3.scale.set(22, 22, 22);
         scene.add(tree3)
       });
 
       // 创建五环
       const fiveCycles = [
-        {
-          key: 'cycle_0',
-          color: 0x0885c2,
-          position: { x: -250, y: 0, z: 0 }
-        },
-        {
-          key: 'cycle_1',
-          color: 0x000000,
-          position: { x: -10, y: 0, z: 5 }
-        },
-        {
-          key: 'cycle_2',
-          color: 0xed334e,
-          position: { x: 230, y: 0, z: 0 }
-        },
-        {
-          key: 'cycle_3',
-          color: 0xfbb132,
-          position: { x: -125, y: -100, z: -5 }
-        },
-        {
-          key: 'cycle_4',
-          color: 0x1c8b3c,
-          position: { x: 115, y: -100, z: 10 }
-        }
+        { key: 'cycle_0', color: 0x0885c2, position: { x: -250, y: 0, z: 0 }},
+        { key: 'cycle_1', color: 0x000000, position: { x: -10, y: 0, z: 5 }},
+        { key: 'cycle_2', color: 0xed334e, position: { x: 230, y: 0, z: 0 }},
+        { key: 'cycle_3', color: 0xfbb132, position: { x: -125, y: -100, z: -5 }},
+        { key: 'cycle_4', color: 0x1c8b3c, position: { x: 115, y: -100, z: 10 }}
       ];
       fiveCycles.map(item => {
         let cycleMesh = new THREE.Mesh(new THREE.TorusGeometry(100, 10, 10, 50), new THREE.MeshLambertMaterial({
           color: new THREE.Color(item.color),
           side: THREE.DoubleSide
         }));
+        cycleMesh.castShadow = true;
         cycleMesh.position.set(item.position.x, item.position.y, item.position.z);
         meshes.push(cycleMesh);
         fiveCyclesGroup.add(cycleMesh);
       });
-      fiveCyclesGroup.scale.set(.03, .03, .03);
-      fiveCyclesGroup.position.set(0, 8, -5);
+      fiveCyclesGroup.scale.set(.036, .036, .036);
+      fiveCyclesGroup.position.set(0, 10, -8);
       scene.add(fiveCyclesGroup);
 
       // 创建雪花
@@ -226,9 +280,12 @@ export default class Olympic extends React.Component {
       controls.enableDamping = true;
       controls.enablePan = false;
       controls.enableZoom = false;
-      // 最大仰角
+      // 垂直旋转角度限制
       controls.minPolarAngle = 1.4;
       controls.maxPolarAngle = 1.8;
+      // 水平旋转角度限制
+      controls.minAzimuthAngle = -.6;
+      controls.maxAzimuthAngle = .6;
       window.addEventListener('resize', onWindowResize, false);
     }
 
@@ -238,17 +295,13 @@ export default class Olympic extends React.Component {
       renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    function onDocumentMouseMove( event ) {
-      mouseX = event.clientX - windowHalfX;
-      mouseY = event.clientY - windowHalfY;
-    }
-
     function animate() {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
       stats && stats.update();
       controls && controls.update();
-      fiveCyclesGroup && (fiveCyclesGroup.rotation.y += .01)
+      TWEEN && TWEEN.update();
+      fiveCyclesGroup && (fiveCyclesGroup.rotation.y += .01);
       let vertices = points.geometry.vertices;
       vertices.forEach(function (v) {
         v.y = v.y - (v.velocityY);
@@ -258,6 +311,8 @@ export default class Olympic extends React.Component {
       });
       // 顶点变动之后需要更新，否则无法实现雨滴特效
       points.geometry.verticesNeedUpdate = true;
+      let time = clock.getDelta();
+      mixer && mixer.update(time);
     }
 
     // 增加点击事件，声明raycaster和mouse变量
@@ -280,7 +335,15 @@ export default class Olympic extends React.Component {
 
   render () {
     return (
-      <div id="container"></div>
+      <div>
+        <div id="container"></div>
+        {this.state.loadingProcess === 100 ? '' : (
+          <div className="olympic_loading">
+            <div className="box">{this.state.loadingProcess} %</div>
+          </div>
+        )
+      }
+      </div>
     )
   }
 }
