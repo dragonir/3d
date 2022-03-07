8986da79d5a9b69afc1577e57e38265fb3131b32
dragonir <dragonirxo@outlook.com>
2022-03-07 18:41:30 +0800

    feat: 🌟浮动字体效果页面样式优化、增加特效开关、文章内容更新.

--- src/containers/Floating/index.js
+++ src/containers/Floating/index.js
@@ -6,10 +6,6 @@ import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
 import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js';
-
-// todo: 双击进入全屏
-// todo: 添加github链接
-
 export default class Earth extends React.Component {
 
   componentDidMount() {
@@ -17,11 +13,13 @@ export default class Earth extends React.Component {
   }
 
   state = {
-    backgroundColor: '#2D6C40'
+    backgroundColor: '#164CCA',
+    // ios 有 bug
+    renderGlithPass: !(window.navigator.userAgent.toLowerCase().indexOf('mobile') > 0)
   }
 
   initThree = () => {
-    var canvas, stats, camera, scene, renderer, glitchPass, composer, mouseX = 0, mouseY = 0;
+    var canvas, stats, camera, scene, renderer, glitchPass, composer, mouseX = 0, mouseY = 0, _this = this;
     const group = new THREE.Group(), textMesh = new THREE.Mesh(), nearDist = 0.1, farDist = 10000;
     init();
     animate();
@@ -44,12 +42,56 @@ export default class Earth extends React.Component {
       glitchPass = new GlitchPass();
       composer.addPass(glitchPass);
 
+      // 页面缩放
       window.addEventListener('resize', () => {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
         renderer.setSize(window.innerWidth, window.innerHeight);
 				composer.setSize( window.innerWidth, window.innerHeight );
       }, false);
+
+      // 双击全屏
+      window.addEventListener('dblclick', () => {
+        const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+        if (!fullscreenElement) {
+          if (canvas.requestFullscreen) {
+            canvas.requestFullscreen();
+          } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen();
+          }
+          console.log('go fullscrenn')
+          scene.background = new THREE.Color(_this.state.backgroundColor)
+        } else {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+          }
+          console.log('leave fullscrenn')
+          scene.background = '';
+          composer.render();
+        }
+      })
+
+      // 字体
+      const loader = new FontLoader();
+      loader.load('./fonts/helvetiker_regular.typeface.json', font => {
+        textMesh.geometry = new TextGeometry('@dragonir\nfantastic\nthree.js\nart work', {
+          font: font,
+          size: 100,
+          height: 40,
+          curveSegments: 12,
+          bevelEnabled: true,
+          bevelThickness: 30,
+          bevelSize: 8,
+          bevelOffset: 1,
+          bevelSegments: 12
+        });
+        textMesh.material = material;
+        textMesh.position.x = 120 * -2;
+        textMesh.position.z = 120 * -1;
+        scene.add(textMesh);
+      });
 
       // BufferAttribute 允许更有效地将数据传递到 GPU
       const octahedronGeometry = new THREE.OctahedronBufferGeometry(80);
@@ -60,34 +102,14 @@ export default class Earth extends React.Component {
       const coneGeometry = new THREE.ConeBufferGeometry(40, 80, 80);
       generateRandomMesh(coneGeometry, material, 100);
       scene.add(group);
-
-      // 字体
-      const loader = new FontLoader();
-      loader.load('./fonts/helvetiker_regular.typeface.json', font => {
-        textMesh.geometry = new TextGeometry('@dragonir\nfantastic\nthree.js\nart page', {
-          font: font,
-          size: 120,
-          height: 120 / 2,
-          curveSegments: 12,
-          bevelEnabled: true,
-          bevelThickness: 10,
-          bevelSize: 6,
-          bevelOffset: 1,
-          bevelSegments: 8
-        });
-        textMesh.material = material;
-        textMesh.position.x = 120 * -2;
-        textMesh.position.z = 120 * -1;
-        scene.add(textMesh);
-      });
     }
 
     const mouseFX = {
       windowHalfX: window.innerWidth / 2,
       windowHalfY: window.innerHeight / 2,
       coordinates: (coordX, coordY) => {
-        mouseX = (coordX - mouseFX.windowHalfX) * 10;
-        mouseY = (coordY - mouseFX.windowHalfY) * 10;
+        mouseX = (coordX - mouseFX.windowHalfX) * 5;
+        mouseY = (coordY - mouseFX.windowHalfY) * 5;
       },
       onMouseMove: e => { mouseFX.coordinates(e.clientX, e.clientY) },
       onTouchMove: e => { mouseFX.coordinates(e.changedTouches[0].clientX, e.changedTouches[0].clientY)}
@@ -131,7 +153,7 @@ export default class Earth extends React.Component {
       textMesh.rotation.y = ry;
       textMesh.rotation.z = rx;
       renderer.render(scene, camera);
-      composer.render();
+      _this.state.renderGlithPass && composer.render();
       stats && stats.update();
     }
   }
@@ -142,13 +164,25 @@ export default class Earth extends React.Component {
     })
   }
 
+  handleRenderChange = () => {
+    this.setState({
+      renderGlithPass: !this.state.renderGlithPass
+    })
+  }
+
   render () {
     return (
       <div className='floating_page' style={{ backgroundColor: this.state.backgroundColor }}>
         <div id="canvas"></div>
         <input className='color_pick' type="color" onChange={this.handleInputChange} value={this.state.backgroundColor} />
+        <button className='pass_button' onClick={this.handleRenderChange}>特效<span className='highlight'>{this.state.renderGlithPass ? '开' : '关'}</span></button>
         <div className='meta'>
-          <p className='text'>author: dragonir</p>
+          <a className="github" href='https://github.com/dragonir/3d' target="_blank" rel="noreferrer">
+            <svg height="32" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true">
+              <path fill='#ffffff' fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+            </svg>
+          </a>
+          <p className='text'>dragonir</p>
           <p className='text'>website: <a href='https://tricell.fun' target="_blank" rel="noreferrer">tricell.fun</a></p>
           <p className='text'>juejin: <a href='https://juejin.cn/user/2295436008498765' target="_blank" rel="noreferrer">@dragonir</a></p>
           <p className='text'>cnblog: <a href='https://www.cnblogs.com/dragonir/' target="_blank" rel="noreferrer">@dragonir</a></p>
