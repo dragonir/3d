b237991126184c0952cff11694c697b6f84adca4
dragonir <dragonirxo@outlook.com>
2022-05-31 19:03:11 +0800

    fix: 🦊 Metaverse 修复操作轮盘层级问题.

--- src/containers/Metaverse/scripts/JoyStick.js
+++ src/containers/Metaverse/scripts/JoyStick.js
@@ -12,6 +12,7 @@ export default class JoyStick {
       transform: translateX(-50%);
       box-sizing: border-box;
       filter: drop-shadow(0px 1px 1px rgba(0, 0, 0, .25));
+      z-index: 11
     `;
     const thumb = document.createElement('div');
     thumb.style.cssText = `
