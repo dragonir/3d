63b1048faab8191c07123df531d2e2aa6389e240
dragonir <dragonirxo@outlook.com>
2022-07-19 10:26:07 +0800

    feat: earthDigital 添加图表和logo.

--- src/containers/EarthDigital/index.js
+++ src/containers/EarthDigital/index.js
@@ -8,9 +8,49 @@ import * as dat from 'dat.gui';
 import imgData from '@/containers/EarthDigital/images/earth.jpg';
 import lineFragmentShader from '@/containers/EarthDigital/shaders/line/fragment.glsl';
 
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core';
+// 引入柱状图图表，图表后缀都为 Chart
+import { BarChart, LineChart, PieChart } from 'echarts/charts';
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  PolarComponent,
+  LegendComponent,
+  ToolboxComponent,
+} from 'echarts/components';
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册必须的组件
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+  BarChart,
+  LabelLayout,
+  UniversalTransition,
+  CanvasRenderer,
+  PolarComponent,
+  LegendComponent,
+  ToolboxComponent,
+  LineChart,
+  PieChart
+]);
+
+
 export default class Earth extends React.Component {
   componentDidMount() {
-    this.initThree()
+    this.initThree();
+    this.initChart();
   }
 
   initThree = () => {
@@ -18,7 +58,7 @@ export default class Earth extends React.Component {
     let scene = new THREE.Scene();
     scene.background = new THREE.Color(0, 0, 0);
     let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 50);
-    camera.position.set(0, 10, 10);
+    camera.position.set(0, 0, 15);
 
     const renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('canvas.webgl'),
@@ -33,14 +73,15 @@ export default class Earth extends React.Component {
 
     let params = {
       colors: {
-        base: "#555fff",
-        gradInner: "#64ff64",
-        gradOuter: "#64ffff"
+        // 点图颜色
+        base: "#f9f002",
+        gradInner: "#8ae66e",
+        gradOuter: "#03c03c"
       },
       reset: () => {controls.reset()}
     }
 
-    let maxImpactAmount = 40;
+    let maxImpactAmount = 10;
     let impacts = [];
     let trails = [];
     for (let i = 0; i < maxImpactAmount; i++) {
@@ -59,7 +100,7 @@ export default class Earth extends React.Component {
       impacts: {value: impacts},
       maxSize: {value: 0.04},
       minSize: {value: 0.03},
-      waveHeight: {value: 0.125},
+      waveHeight: {value: 0.1},
       scaling: {value: 2},
       gradInner: {value: new THREE.Color(params.colors.gradInner)},
       gradOuter: {value: new THREE.Color(params.colors.gradOuter)}
@@ -105,20 +146,20 @@ export default class Earth extends React.Component {
 
     const gui = new dat.GUI();
     console.log(gui)
-    gui.add(uniforms.maxSize, "value", 0.01, 0.06).step(0.001).name("land");
-    gui.add(uniforms.minSize, "value", 0.01, 0.06).step(0.001).name("ocean");
-    gui.add(uniforms.waveHeight, "value", 0.1, 1).step(0.001).name("waveHeight");
-    gui.add(uniforms.scaling, "value", 1, 5).step(0.01).name("scaling");
-    gui.addColor(params.colors, "base").name("base color").onChange(val => {
+    gui.add(uniforms.maxSize, 'value', 0.01, 0.06).step(0.001).name('land');
+    gui.add(uniforms.minSize, 'value', 0.01, 0.06).step(0.001).name('ocean');
+    gui.add(uniforms.waveHeight, 'value', 0.1, 1).step(0.001).name('waveHeight');
+    gui.add(uniforms.scaling, 'value', 1, 5).step(0.01).name('scaling');
+    gui.addColor(params.colors, 'base').name('base color').onChange(val => {
       if (o) o.material.color.set(val);
     });
-    gui.addColor(params.colors, "gradInner").name("inner").onChange(val => {
+    gui.addColor(params.colors, 'gradInner').name('inner').onChange(val => {
       uniforms.gradInner.value.set(val);
     });
-    gui.addColor(params.colors, "gradOuter").name("outer").onChange(val => {
+    gui.addColor(params.colors, 'gradOuter').name('outer').onChange(val => {
       uniforms.gradOuter.value.set(val);
     });
-    gui.add(params, "reset").name("Reset Controls");
+    gui.add(params, 'reset').name('Reset Controls');
     gui.close();
     // gui.hide();
 
@@ -259,7 +300,8 @@ export default class Earth extends React.Component {
       o.rotation.y = Math.PI;
       trails.forEach(t => {o.add(t)});
       o.add(new THREE.Mesh(new THREE.SphereGeometry(4.9995, 72, 36), new THREE.MeshBasicMaterial({color: scene.background})));
-      // scene.add(o);
+      o.position.set(0, -.4, 0);
+      scene.add(o);
     }
 
     function makeTrail(idx){
@@ -326,20 +368,317 @@ export default class Earth extends React.Component {
     }
   }
 
+  initChart = () => {
+    // const chart_0 = echarts.init(document.getElementsByClassName('chart_0')[0], 'dark');
+    // chart_0 && chart_0.setOption({
+    //   tooltip: {},
+    //   backgroundColor: 'transparent',
+    //   dataset: {
+    //     source: [
+    //       ['', '2012', '2013', '2014', '2015', '2016', '2017'],
+    //       ['', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
+    //       ['', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
+    //       ['', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
+    //       ['', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
+    //     ]
+    //   },
+    //   series: [
+    //     {
+    //       type: 'pie',
+    //       radius: '40%',
+    //       center: ['25%', '30%'],
+    //       labelLine: {
+    //         show: false
+    //       },
+    //     },
+    //     {
+    //       type: 'pie',
+    //       radius: '40%',
+    //       center: ['75%', '30%'],
+    //       encode: {
+    //         itemName: 'product',
+    //         value: '2013'
+    //       },
+    //       labelLine: {
+    //         show: false
+    //       }
+    //     },
+    //     {
+    //       type: 'pie',
+    //       radius: '40%',
+    //       center: ['25%', '75%'],
+    //       encode: {
+    //         itemName: 'product',
+    //         value: '2014'
+    //       },
+    //       labelLine: {
+    //         show: false
+    //       }
+    //     },
+    //     {
+    //       type: 'pie',
+    //       radius: '40%',
+    //       center: ['75%', '75%'],
+    //       encode: {
+    //         itemName: 'product',
+    //         value: '2015'
+    //       },
+    //       labelLine: {
+    //         show: false
+    //       }
+    //     }
+    //   ]
+    // });
+
+    const chart_1 = echarts.init(document.getElementsByClassName('chart_1')[0], 'dark');
+    chart_1 && chart_1.setOption({
+      backgroundColor: 'transparent',
+      textStyle: {
+        color: '#f9f002'
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          color: '#000'
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: 'Email',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+          name: 'Union Ads',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+          name: 'Video Ads',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: [150, 232, 201, 154, 190, 330, 410]
+        },
+        {
+          name: 'Direct',
+          type: 'line',
+          stack: 'Total',
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: [320, 332, 301, 334, 390, 330, 320]
+        },
+        {
+          name: 'Search Engine',
+          type: 'line',
+          stack: 'Total',
+          label: {
+            show: true,
+            position: 'top'
+          },
+          areaStyle: {},
+          emphasis: {
+            focus: 'series'
+          },
+          data: [820, 932, 901, 934, 1290, 1330, 2400]
+        }
+      ]
+    });
+
+    const chart_2 = echarts.init(document.getElementsByClassName('chart_2')[0], 'dark');
+    chart_2 && chart_2.setOption({
+      backgroundColor: 'transparent',
+      textStyle: {
+        color: '#000000'
+      },
+      series: [
+        {
+          name: 'Nightingale Chart',
+          type: 'pie',
+          radius: [25, 100],
+          center: ['50%', '50%'],
+          roseType: 'area',
+          itemStyle: {
+            borderRadius: 4
+          },
+          data: [
+            { value: 40, name: 'rose 1' },
+            { value: 38, name: 'rose 2' },
+            { value: 32, name: 'rose 3' },
+            { value: 30, name: 'rose 4' },
+            { value: 28, name: 'rose 5' },
+            { value: 26, name: 'rose 6' },
+            { value: 22, name: 'rose 7' },
+            { value: 18, name: 'rose 8' }
+          ],
+          label: {
+            color: '#000000'
+          }
+        }
+      ]
+    });
+
+    const chart_3 = echarts.init(document.getElementsByClassName('chart_3')[0], 'dark');
+    chart_3 && chart_3.setOption({
+      backgroundColor: 'transparent',
+      tooltip: {},
+      dataset: {
+        source: [
+          ['', '2012', '2013', '2014', '2015', '2016', '2017'],
+          ['Milk Tea', 86.5, 92.1, 85.7, 83.1, 73.4, 55.1],
+          ['Matcha Latte', 41.1, 30.4, 65.1, 53.3, 83.8, 98.7],
+          ['Cheese Cocoa', 24.1, 67.2, 79.5, 86.4, 65.2, 82.5],
+          ['Walnut Brownie', 55.2, 67.1, 69.2, 72.4, 53.9, 39.1]
+        ]
+      },
+      series: [
+        {
+          type: 'pie',
+          radius: '20%',
+          center: ['25%', '30%']
+        },
+        {
+          type: 'pie',
+          radius: '20%',
+          center: ['75%', '30%'],
+          encode: {
+            itemName: 'product',
+            value: '2013'
+          }
+        },
+        {
+          type: 'pie',
+          radius: '20%',
+          center: ['25%', '75%'],
+          encode: {
+            itemName: 'product',
+            value: '2014'
+          }
+        },
+        {
+          type: 'pie',
+          radius: '20%',
+          center: ['75%', '75%'],
+          encode: {
+            itemName: 'product',
+            value: '2015'
+          }
+        }
+      ]
+    });
+
+    const chart_4 = echarts.init(document.getElementsByClassName('chart_4')[0], 'dark');
+    chart_4 && chart_4.setOption({
+      backgroundColor: 'transparent',
+      angleAxis: {
+        max: 2,
+        startAngle: 30,
+        splitLine: {
+          show: false
+        }
+      },
+      radiusAxis: {
+        type: 'category',
+        data: ['v', 'w', 'x', 'y', 'z'],
+        z: 10
+      },
+      polar: {},
+      series: [
+        {
+          type: 'bar',
+          data: [4, 3, 2, 1, 0],
+          coordinateSystem: 'polar',
+          name: 'Without Round Cap',
+          itemStyle: {
+            borderColor: 'red',
+            opacity: 0.8,
+            borderWidth: 1
+          }
+        },
+        {
+          type: 'bar',
+          data: [4, 3, 2, 1, 0],
+          coordinateSystem: 'polar',
+          name: 'With Round Cap',
+          roundCap: true,
+          itemStyle: {
+            borderColor: 'green',
+            opacity: 0.8,
+            borderWidth: 1
+          }
+        }
+      ]
+    });
+
+    const chart_5 = echarts.init(document.getElementsByClassName('chart_5')[0], 'dark');
+    chart_5 && chart_5.setOption({
+      backgroundColor: 'transparent',
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar'
+        }
+      ]
+    });
+  }
+
   render () {
     return (
       <div className='earth_digital'>
         <canvas className='webgl'></canvas>
-        <header className='hud header'></header>
+        <header className='hud header'>
+          <div className='left'></div>
+          <div className='middle'></div>
+          <div className='right'></div>
+        </header>
+        <div className='logo'></div>
         <aside className='hud aside left'>
-          <div className='box'></div>
-          <div className='box inverse'></div>
-          <div className='box inverse dotted'></div>
+          <div className='box box_0 inverse'>
+            <div className='cover'></div>
+            <p className='info'>Cyberpunk is a subgenre of science fiction in a dystopian futuristic setting that tends to focus on a "combination of lowlife and high tech", featuring futuristic technological and scientific achievements, such as artificial intelligence and cybernetics, juxtaposed with societal collapse or decay. </p>
+          </div>
+          <div className='box'><div className='chart chart_1'></div></div>
+          <div className='box inverse dotted'><div className='chart chart_2'></div></div>
         </aside>
         <aside className='hud aside right'>
-          <div className='box'></div>
-          <div className='box inverse'></div>
-          <div className='box inverse dotted'></div>
+          <div className='box'><div className='chart chart_3'></div></div>
+          <div className='box'><div className='chart chart_4'></div></div>
+          <div className='box inverse dotted'><div className='chart chart_5'></div></div>
         </aside>
         <footer className='hud footer'>
         </footer>
