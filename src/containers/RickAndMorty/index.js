a8d6acf7c327c57f819c22a759e1559acdaa9ed3
dragonir <dragonirxo@outlook.com>
2022-11-15 19:48:22 +0800

    feat: 🛸 Rick And Morty页面样式优化.

--- src/containers/RickAndMorty/index.js
+++ src/containers/RickAndMorty/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
-import * as dat from 'dat.gui';
+// import * as dat from 'dat.gui';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
@@ -180,20 +180,20 @@ export default class RickAndMorty extends React.Component {
     bloomComposer.addPass(bloomPass);
 
     // dat.gui
-    const gui = new dat.GUI();
-    const bloom = gui.addFolder('bloom');
-    bloom.add(options, 'bloomStrength', 0.0, 5.0).name('bloomStrength').listen();
-    bloom.add(options, 'bloomRadius', .1, 2.0).name('bloomRadius').listen();
-    bloom.open();
-    const colors = gui.addFolder('Colors');
-    colors.addColor(options, 'color0').name('layer0');
-    colors.addColor(options, 'color1').name('layer1');
-    colors.addColor(options, 'color2').name('layer2');
-    colors.addColor(options, 'color3').name('layer3');
-    colors.addColor(options, 'color4').name('layer4');
-    colors.addColor(options, 'color5').name('layer5');
-    colors.open();
-    gui.hide();
+    // const gui = new dat.GUI();
+    // const bloom = gui.addFolder('bloom');
+    // bloom.add(options, 'bloomStrength', 0.0, 5.0).name('bloomStrength').listen();
+    // bloom.add(options, 'bloomRadius', .1, 2.0).name('bloomRadius').listen();
+    // bloom.open();
+    // const colors = gui.addFolder('Colors');
+    // colors.addColor(options, 'color0').name('layer0');
+    // colors.addColor(options, 'color1').name('layer1');
+    // colors.addColor(options, 'color2').name('layer2');
+    // colors.addColor(options, 'color3').name('layer3');
+    // colors.addColor(options, 'color4').name('layer4');
+    // colors.addColor(options, 'color5').name('layer5');
+    // colors.open();
+    // gui.hide();
 
     const loadingManager = new THREE.LoadingManager();
     loadingManager.onProgress = async(url, loaded, total) => {};
@@ -253,6 +253,7 @@ export default class RickAndMorty extends React.Component {
     return (
       <div className='rick_and_morty_page'>
         <canvas className='webgl'></canvas>
+        <i className="logo"></i>
         <a className='github' href='https://github.com/dragonir/3d' target='_blank' rel='noreferrer'>
           <svg height='36' aria-hidden='true' viewBox='0 0 16 16' version='1.1' width='36' data-view-component='true'>
             <path fill='#FFFFFF' fillRule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
